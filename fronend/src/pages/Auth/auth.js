6120d000407d9458213d79e714185845e118808d
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2025-04-20 11:41:43 -0300

    arrumei o front do auth

--- fronend/src/pages/Auth/auth.js
+++ fronend/src/pages/Auth/auth.js
@@ -1,3 +1,5 @@
+import { auth,signInWithEmailAndPassword,GoogleAuthProvider,provider,signInWithPopup,providerMicrosoft,OAuthProvider } from "../../../../backend/src/config/firebaseConfig.js";
+
  // Script para alternar entre os formulários de login e cadastro
  document.getElementById('show-signup').addEventListener('click', function(e) {
     e.preventDefault();
@@ -19,4 +21,23 @@ document.getElementById('valluo-logo').addEventListener('click', function() {
 
 document.getElementById('valluo-logo-signup').addEventListener('click', function() {
     window.location.href = '../home';
+});
+
+// Google
+const googleButton = document.getElementById('googleLogin');
+googleButton.addEventListener("click", function(event) {
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    window.location.href = "dashboard2.html";
+
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+
 });
